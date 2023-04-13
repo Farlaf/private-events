51591f789a297f223b0eba668aa6d457ed4edd0b
Farlaf <themehills@gmail.com>
2023-04-13 11:08:32 +0400

    add def avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,25 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one_attached :avatar
+  after_commit :add_default_avatar, on: %i[create update]
 
   def avatar_thubnail
     # avatar.variant(resize: "150x150!").processed if avatar.attached?
     if avatar.attached?
-      avatar.variant(resize_to_limit: [100,100]).processed
-    else
-      "/default_profile.jpg"
+      avatar.variant(resize_to_limit: [40,40]).processed
+    end
+  end
+
+  private
+
+  def add_default_avatar
+    unless avatar.attached?
+      avatar.attach(
+        io: File.open(
+          Rails.root.join('app', 'assets', 'images',  "default_profile.jpg")),
+          filename:  "default_profile.jpg",
+          content_type: 'image/jpg'
+        )
     end
   end
 end
