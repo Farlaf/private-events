bf160b2e3d412497adc243ef51548f76c3429faf
Farlaf <themehills@gmail.com>
2023-04-13 10:19:46 +0400

    add avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_one_attached :avatar
+
+  def avatar_thubnail
+    # avatar.variant(resize: "150x150!").processed if avatar.attached?
+    if avatar.attached?
+      avatar.variant(resize_to_limit: [100,100]).processed
+    else
+      "/default_profile.jpg"
+    end
+  end
 end
